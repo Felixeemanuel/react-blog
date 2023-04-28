7ba4a6234994c4c51f2748af0e3f10cb193553fa
Felix Edström <felix.edstrom1@gmail.com>
2023-04-28 15:54:54 +0200

    first commit

--- src/pages/CreateBlog.jsx
+++ src/pages/CreateBlog.jsx
@@ -1,20 +1,37 @@
 import React from 'react'
+import { useState } from 'react'
 
 function CreateBlog() {
+
+    const [inputs, setInputs] = useState('')
+
+    const handleChange = (e) => {
+        const name = e.target.name
+        const value = e.target.value
+
+        setInputs(values => ({...values, [name]: value}))
+    }
+
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        console.log(inputs)
+
+    }
   return (
     <>
     <div className='form-page'>
-        <form className="form-container">
+        <form onSubmit={handleSubmit} className="form-container">
             <h2>Create a new Blog!</h2>
             <div className="form-control">
                 <label htmlFor="title" className="label">Title:</label>
-                <input type="text" id='title' className="input" />
+                <input onChange={handleChange} name='title' type="text" id='title' className="input" />
             </div>
             <div className="form-control">
                 <label htmlFor="textarea" className="label">Body:</label>
-                <textarea name="textarea" id="textarea"></textarea>
+                <textarea onChange={handleChange} name="body" id="textarea"></textarea>
             </div>
-            <button className="submit">Create</button>
+            <button type='submit' className="submit">Create</button>
         </form>
     </div>
     </>
